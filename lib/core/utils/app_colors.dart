36c471d2674683fde46b87bb0b3e4d24739afcbc
Mahmoud Beeko <mahmoud.m.bakir@gmail.com>
2025-07-02 04:00:43 +0300

    Fix color definition for grey in AppColors by correcting the hex value format. This change ensures proper color rendering in the application.

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // Primary colors
   static const Color primary = Colors.deepPurple;
   static const Color darkBlue = Color(0xFF021C4D);
 
   // Neutral colors
   static const Color white = Colors.white;
-  static const Color grey = Color(0xFFC7C7C7);
+  static const Color grey = Color(0x0fc7c7c7);
 }
